f4e031dd160b87ba5ed1d690f130025d4d42e163
danivaknin011-max <danivaknin011@gmail.com>
2026-02-09 10:41:35 +0200

    Update app.py and add flak-app folder

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello from Flask 🚀"
 
+@app.route("/ai")
+def ai():
+    return "Welcome to the AI section!"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
